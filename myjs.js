4b794c4ef2178b351fb5444703fe7d25e963f23b
VoHoangDuong <duong6046237@gmail.com>
2021-03-10 21:48:13 +0700

    sudungtoantu

---
+++ myjs.js
@@ -0,0 +1,4 @@
+let inputWidth = parseInt(prompt("Nhập chiều cao:"));
+let inputHeight = parseInt(prompt("Nhập vào chiều rộng:"));
+let area = inputWidth * inputHeight;
+document.write("Diện tích hình chữ nhật là :" + " " + area);
